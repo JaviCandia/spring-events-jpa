b7689dcf10a4c9bda63320ba9876726cea6c6386
Javier Candia <javi-candia@hotmail.com>
2025-12-19 17:22:03 -0600

    Event entity added

---
+++ src/main/java/com/javiersillo/eventosapi/domain/Event.java
@@ -0,0 +1,19 @@
+package com.javiersillo.eventosapi.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "events")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDate date;
+    private String location;
+}
