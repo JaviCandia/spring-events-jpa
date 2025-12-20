a96c8a52460a1e76fb757616ef1864353ed4fec8
Javier Candia <javi-candia@hotmail.com>
2025-12-20 00:57:21 -0600

    repository added

---
+++ src/main/java/com/javiersillo/eventosapi/repository/EventRepository.java
@@ -0,0 +1,8 @@
+package com.javiersillo.eventosapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Repository, Long> {
+}
